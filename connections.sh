46ea2c483f93527a81958c1e0c1ff1087326a25f
Yingzhao Ouyang <KunojiLym@gmail.com>
2021-05-23 16:26:26 +0800

    Added sudo to docker commands

--- connections.sh
+++ connections.sh
@@ -5,5 +5,5 @@ curl --user airflow:airflow -d @psql-origin.json -H "Content-Type: application/j
 curl --user airflow:airflow -d @psql-dest.json -H "Content-Type: application/json" -X POST http://localhost:5884/api/v1/connections
 
 # 2.2 Reload Airflow
-docker compose -f airflow/docker-compose.yaml down
-docker compose -f airflow/docker-compose.yaml up --add-host=host.docker.internal:host-gateway --detach
+sudo docker compose -f airflow/docker-compose.yaml down
+sudo docker compose -f airflow/docker-compose.yaml up --add-host=host.docker.internal:host-gateway --detach
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # 1. Deploy Postgres DBs as a Docker stack
-docker compose up --detach
+sudo docker compose up --detach
 
 # 2. Build and deploy Airflow
-docker compose -f airflow/docker-compose.yaml up airflow-init
-docker compose -f airflow/docker-compose.yaml up --add-host=host.docker.internal:host-gateway --detach
+sudo docker compose -f airflow/docker-compose.yaml up airflow-init
+sudo docker compose -f airflow/docker-compose.yaml up --add-host=host.docker.internal:host-gateway --detach
